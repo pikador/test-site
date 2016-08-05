b275f7a75cfa11d7c566ba99194d67522f3c4314
Pikadorinka <artsemovich@gmail.com>
2016-08-05 15:14:28 +0700

    пятый коммит

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var mySlider;
+var value;
+$(document).ready(function () {
+    mySlider = $("input.slider-money").bootstrapSlider();
+    //value = mySlider.bootstrapSlider('getValue');
+    //mySlider
+        //.bootstrapSlider('setValue', 5);
+
+
+})
